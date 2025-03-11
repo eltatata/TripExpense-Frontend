910ef65c8e64453d430a85aceff05883d913e9d8
Diego Salazar <diego.salazarc4@gmail.com>
2025-03-11 12:14:02 -0500

    Add sections for explore flights and hotels and also add a footer

--- trip-expense-front/src/components/LandingPage.jsx
+++ trip-expense-front/src/components/LandingPage.jsx
@@ -4,6 +4,7 @@ import HeroSection from "./HeroSection";
 import SearchForm from "./SearchForm";
 import "./LandingPage.css";
 import Destinations from "./Destinations";
+import FlightsHotels from "./FlightsHotels";
 
 const LandingPage = () => {
     return (
@@ -15,6 +16,12 @@ const LandingPage = () => {
             </div>
             <div className="destinations__div">
                 <Destinations/>
+            </div>
+            <div className="flightshotels__div">
+                <FlightsHotels/>
+            </div>
+            <div className="footer__copyright">
+                <p>&copy; {new Date().getFullYear()} TripExpense. Todos los derechos reservados.</p>
             </div>
         </div>
     );
